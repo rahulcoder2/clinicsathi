79720a0405b319dace6589ab9707dd7076bc2b47
Saphalya Acharya <programmersaphal@gmail.com>
2024-12-21 14:11:38 +0545

    add: reset password

--- serverside/config/registration/urls.py
+++ serverside/config/registration/urls.py
@@ -3,8 +3,21 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import UserRegistrationView, CustomTokenObtainPairView, PatientRegistrationView, DoctorRegistrationView, ProtectedView, LogoutView, UnregisteredUserView, AppointmentBookingView, DoctorManageAppointmentsView, PatientCompleteAppointmentView
-
+from .views import (
+    UserRegistrationView, 
+    CustomTokenObtainPairView, 
+    PatientRegistrationView, 
+    DoctorRegistrationView, 
+    ProtectedView, 
+    LogoutView, 
+    UnregisteredUserView, 
+    AppointmentBookingView, 
+    DoctorManageAppointmentsView, 
+    PatientCompleteAppointmentView,
+    AppointmentRescheduleView,
+    PasswordResetConfirmView,
+    PasswordResetRequestView,
+)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
@@ -15,9 +28,12 @@ urlpatterns = [
     path('protected/', ProtectedView.as_view(), name='protected'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('restricted/', UnregisteredUserView.as_view(), name='restricted'),
-    path('book/', AppointmentBookingView.as_view(), name='book-appointment'),
     path('appointments/book/', AppointmentBookingView.as_view(), name='book-appointment'),
+    path('appointments/reschedule/<int:appointment_id>/', AppointmentRescheduleView.as_view(), name='reschedule-appointment'),
     path('appointments/manage/', DoctorManageAppointmentsView.as_view(), name='manage-appointments'),
     path('appointments/manage/<int:appointment_id>/', DoctorManageAppointmentsView.as_view(), name='update-appointment'),
     path('appointments/complete/<int:appointment_id>/', PatientCompleteAppointmentView.as_view(), name='complete-appointment'),
+    path('password-reset/request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
+    path('password-reset/confirm/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+
 ]
